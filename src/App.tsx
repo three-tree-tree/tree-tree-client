7a699d6f43565dcf1a1bf852eafddbb98ca43473
HoonBaek <hoon@sendbird.com>
2023-02-20 09:54:08 +0900

    First hello world of this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          안녕하세요 재직톤 6팀의 Tree Tree 입니다
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.notion.so/6-1d67c1cc375c42eda3e5fe034427845a"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          재직톤 노션 페이지
         </a>
       </header>
     </div>
