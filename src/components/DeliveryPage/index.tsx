1d13a9623ed300b7eb49dd5ae9c4dc7f9a4d34f0
yool Han <yr.han@bucketplace.net>
2023-02-26 16:31:51 +0900

    마지막 페이지 버튼 구현

--- src/components/DeliveryPage/index.tsx
+++ src/components/DeliveryPage/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
 import './index.css';
 
 import { Icon, IconTypes } from '../Icon';
@@ -11,6 +13,7 @@ import ImageGuide from './image-guide.svg'
 import TagTexts from './tag-texts.svg';
 
 const INSTAGRAM_FEED_URL = "https://www.instagram.com/explore/tags/%EC%A7%80%EA%B5%AC%EB%B0%A9%EC%9C%84%EB%8C%80_%EC%A7%80%EA%B5%AC%EB%A5%BC%EC%A7%80%EC%BC%9C%EB%9D%BC/"
+const SERVICE_HOST = "https://three-tree-tree.github.io/"
 
 interface DeliveryPageProps {
   className?: string;
@@ -65,15 +68,16 @@ export const DeliveryPage = ({
       <div className='tree-delivery-page__button-wrapper'>
         <Button
           className='tree-delivery-page__retry'
-          onClick={() => {}}
+          onClick={() => {setPage(PageStatus.LANDING)}}
           type={ButtonTypes.RETRY}
         />
-
+      <CopyToClipboard text={SERVICE_HOST} onCopy={() => {}}>
         <Button
           className='tree-delivery-page__copy'
           onClick={() => {}}
           type={ButtonTypes.COPY_LINK}
           />
+        </CopyToClipboard>
       </div>
       {/* { openPopUp && <CompletePopup className="tree-delivery-page__popup-modal" />} */}
     </div>
