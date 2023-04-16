cc31bf09c2234f742982ea8b7d4c06d75b4cfadd
yool Han <yr.han@bucketplace.net>
2023-04-16 17:20:22 +0900

    음수에서 시작되지 않도록 수정

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { GlobalContext, PageStatus, useGlobalContext } from '../../lib/GlobalContext';
-import { Button, ButtonTypes } from '../Button';
-import { Icon, IconTypes } from '../Icon';
 import axios from 'axios';
 import CountUp from 'react-countup';
 
@@ -9,7 +7,6 @@ import TopLogo from './top_logo.svg';
 import Earth from './earth.svg';
 import Sparkle from './sparkle.png';
 import IcNextButton from '../Icon/next-button.svg'
-import Superman from './superman.svg';
 
 import './index.css';
 
@@ -24,7 +21,7 @@ export const LandingPage = ({
   className,
 }: LandingPageProps): React.ReactElement => {
   const { setPage } = useGlobalContext() as GlobalContext;
-  const [peopleCount, setPeopleCount] = useState(0);
+  const [peopleCount, setPeopleCount] = useState(undefined);
 
   useEffect(() => {
     axios
@@ -44,74 +41,14 @@ export const LandingPage = ({
     setPage(PageStatus.PROMISE);
   }
 
-  // return (
-  //   <div className={`${className} tree-landing-page`}>
-  //     <div className='tree-landing-page__people-div' style={{ visibility: peopleCount ? "visible" : "hidden" }}>
-  //       <Icon
-  //         className='tree-landing-page__title-logo'
-  //         type={IconTypes.CURRENT_COUNT}
-  //       />
-  //       <div>
-  //         <div className='tree-landing-page__people-count'> {peopleCount}명 </div>
-  //       </div>
-  //       <div className='tree-landing-page__main-content'>
-  //         <MainContent />
-  //       </div>
-  //     </div>
-  //     <Button
-  //       className='tree-landing-page__start-button'
-  //       onClick={() => {
-  //         setPage(PageStatus.SELECTING)
-  //         console.log('click to Selection')
-  //       }}
-  //       type={ButtonTypes.START_JOIN}
-  //     />
-  //     <Button
-  //       className='tree-landing-page__feed-button'
-  //       onClick={handleOnClickFeedButton}
-  //       type={ButtonTypes.INSTAGRAM_FEED}
-  //     />
-  //     <div className='tree-landing-page__text-content'>
-  //       <TextContent />
-  //     </div>
-  //   </div>
-  // );
-
-  // return (
-  //   <div className={`${className} tree-landing-page`}>
-  //     <div className='tree-landing-page__top-logo'>
-  //       <TopLogo />
-  //     </div>
-  //     <div className='tree-landing-page__people-count-wrapper'>
-  //       <div>현재까지 합류한 그린 가디언즈</div>
-  //       <div className='tree-landing-page__people-count'>
-  //         <CountUp 
-  //           start={peopleCount - 20} 
-  //           end={peopleCount}
-  //           duration={7}
-  //           suffix='명'
-  //         />
-  //       </div>
-  //     </div>
-  //     <div className='tree-landing-page__main-logo'>
-  //       <div className='tree-landing-page__main-logo__earth'>
-  //         <Earth />
-  //       </div>
-  //     </div>
-
-  //     <div className='tree-landing-page__content'>
-  //     우리의 아름다운 별, 지구가 아파하고 있어요. <br />
-  //     지구를 지키기 위한 그린 가디언즈가 되어주세요!
-  //     </div>
-
-  //     <div className='tree-landing-page__start-button' onClick={handleOnClickJoinButton}>
-  //         그린 가디언즈 합류하기
-  //       <span className='tree-landing-page__start-button__gt'>&gt;</span>
-  //     </div>
-
-  //     <div className='tree-landing-page__sparkle-bg' />
-  //   </div>
-  // )
+  const getPeopleStartCount = () => {
+    if (peopleCount == undefined) return 0
+    if(peopleCount > 20) {
+      return peopleCount - 20
+    } else {
+      return 0
+    }
+  }
 
   return (
     <div className='`${className} landing-page__container'>
@@ -120,19 +57,26 @@ export const LandingPage = ({
       </div>
       <div className='landing-page__people-count-wrap'>
         <p>현재까지 합류한 그린 가디언즈</p>
-        <CountUp 
-            start={peopleCount - 20} 
+        {
+          peopleCount &&
+          <CountUp 
+            start={getPeopleStartCount()} 
             end={peopleCount}
             duration={7}
             suffix='명'
           />
+        }
+        {
+          peopleCount == undefined &&
+          "0명"
+        }
       </div>
       <div className='landing-page__main-logo'>
         <Earth />
         <img className='landing-page__main-logo-bg' src={Sparkle} />
       </div>
       <p className='landing-page__content'>
-        우리의 아름다운 별, 지구가 아파하고 있어요.<br></br>
+        우리의 아름다운 별, 지구가 아파하고 있어요.<br />
         지구를 지키기 위한 그린 가디언즈가 되어주세요!
       </p>
       <div className='landing-page__start-btn' onClick={handleOnClickJoinButton}>
